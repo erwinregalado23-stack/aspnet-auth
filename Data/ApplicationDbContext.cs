c2ffd018d4a17858f68d5fb72634bfb3eb7d87c7
erwinregalado23-stack <erwin.regalado23@gmail.com>
2026-09-24 05:36:31 +0000

    Add login and sign up.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnet_auth.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
